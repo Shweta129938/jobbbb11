28abc1521061b7b41f064b839fcccbb157b540c7
Shweta129938 <maneshwe1121@gmail.com>
2026-03-08 12:52:59 +0530

    Update aa.java

--- Redbus_automation/src/test/java/ssss/aa.java
+++ Redbus_automation/src/test/java/ssss/aa.java
@@ -8,8 +8,9 @@ import org.testng.annotations.Test;
 public class aa {
 	@Test
 	public void main(){
-		WebDriver driver =new FirefoxDriver();
+		WebDriver driver =new FirefoxDriver();change here u also check it on chromebrowser0
 		driver.get("https://www.redbus.in/");
 	}
 
 }
+
